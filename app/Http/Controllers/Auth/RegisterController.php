54f224e682513c2f4b2229650472dc14b1709586
laFreeFall <name.me.free.fall@gmail.com>
2018-08-02 04:08:47 +0300

    Implement backend user registration and retrieving its data functionality

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -68,5 +69,22 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        if ($request->wantsJson()) {
+            $token = $user->createToken('user_id', [''])->accessToken;
+            return response()->json(compact('user', 'token'), 201);
+        }
+
+        return false;
     }
 }
